ee7cf915c9023fcf899740bbe1d64aaa21218be3
Magnus Hallqvist <magnus.hallqvist@sonymobile.com>
2012-11-14 16:12:33 +0100

    WebViewCore destructor must delete m_textFieldInitDataGlue
    There was a memory leak detected in processes using webkit.
    Investigation showed that it was the TextFieldInitDataGlue
    object created in WebViewCore constructor.
    
    Change-Id: I86a11f99f13aca6ab990627a9106c9e9b781edee

--- Source/WebKit/android/jni/WebViewCore.cpp
+++ Source/WebKit/android/jni/WebViewCore.cpp
@@ -570,6 +570,7 @@ WebViewCore::~WebViewCore()
         m_javaGlue->m_obj = 0;
     }
     delete m_javaGlue;
+    delete m_textFieldInitDataGlue;
 }
 
 WebViewCore* WebViewCore::getWebViewCore(const WebCore::FrameView* view)
